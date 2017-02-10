bcd5b47c36d9842a353cf266d4e13c4d1a18ca65
EA <emashliles@gmail.com>
2017-02-10 14:26:04 +0000

    Add basic tests.

--- src/PrimeFactors.java
+++ src/PrimeFactors.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrimeFactors {
+    public static List<Integer> calculate(int number) {
+        List<Integer> primeFactors = new ArrayList<>();
+
+        if(number == 2 || number == 3) {
+            primeFactors.add(number);
+        }
+        else {
+            primeFactors.add(2);
+            primeFactors.add(2);
+        }
+        return primeFactors;
+    }
 }
--- test/PrimeFactorsTest.java
+++ test/PrimeFactorsTest.java
@@ -1,2 +1,20 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PrimeFactorsTest {
+    @Test
+    public void calculates2() {
+        assertEquals(true, PrimeFactors.calculate(2).contains(2));
+    }
+
+    @Test
+    public void calculates3() {
+        assertEquals(true, PrimeFactors.calculate(3).contains(3));
+    }
+
+    @Test
+    public void calculates4() {
+        assertEquals(2, PrimeFactors.calculate(4).size());
+    }
 }
